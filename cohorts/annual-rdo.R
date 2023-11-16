99fbfcfb39986bf1ae72f9405c49174041313cd1
AHolder1 <Anna.Holder@Waterboards.ca.gov>
2023-11-16 15:24:42 -0800

    move legend to the side of rdo graph

--- cohorts/annual-rdo.R
+++ cohorts/annual-rdo.R
@@ -9,7 +9,6 @@ rdo_long$RDO <- factor(rdo_long$RDO, levels=rev(unique(rdo_long$RDO)))
 
 ggplot(rdo_long, aes(fill=Cohort, x=Count, y=RDO)) + 
   geom_bar(position="stack", stat="identity") +
-  theme(legend.position = "top") +
   xlab("Number of Individuals") + 
   ylab("Water Boards Region, Division, or Office (RDO)") +
   scale_fill_viridis(discrete = TRUE, option = "viridis")
